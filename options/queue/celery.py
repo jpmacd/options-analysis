c7aedeb2d35f13a5584a959f2c6ed0cce2091933
jpmacd <github@0x00.tel>
2025-01-12 14:32:18 -0500

    wip

--- options/queue/celery.py
+++ options/queue/celery.py
@@ -5,3 +5,5 @@ app = Celery("tasks", broker=getenv("CELERY_BROKER_URL"))
 
 app.conf.result_backend = getenv("CELERY_RESULTS_URL")
 app.conf.broker_connection_retry_on_startup = True
+app.conf.worker_pool = "eventlet"
+app.conf.worker_concurrency = 1
